d8c57bcbdfaa7ccf2b04e13a18cf6cc32dae4b19
abc <abc@example.com>
2022-05-30 13:37:25 +0200

    performance tuning

--- src/lib.rs
+++ src/lib.rs
@@ -156,8 +156,8 @@ impl Universe {
     pub fn new() -> Universe {
         utils::set_panic_hook();
 
-        let width = 256;
-        let height = 256;
+        let width = 80;
+        let height = 80;
 
         let cells = (0..width * height)
             .map(|_i| {
